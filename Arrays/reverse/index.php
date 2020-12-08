da167348d6455c5c337912a9f8fa469cb4becc41
Dilshod Khudoyarov <DH.17@mail.ru>
2020-12-08 20:49:57 +0500

    Update index.php

--- Arrays/reverse/index.php
+++ Arrays/reverse/index.php
@@ -14,5 +14,33 @@
  $str = 'Dishkan';
  reverseStr($str);
  
+ 
+echo "<br />";
+echo "<b>Without any commands of php</b>";
+echo "<br />";
+
+function reverseArray(&$arr, $left, $right)
+{
+    while ($left < $right)
+    {
+        $temp = $arr[$left]; 
+        $arr[$left] = $arr[$right];
+        $arr[$right] = $temp;
+        $left++;
+        $right--;
+    }
+
+    for ($i = 0; $i < count($arr); $i++) {
+    echo $arr[$i];	
+	}
+}     
+ 
+$arr = ["h","e","l","l","o"];
+ 
+echo "Reversed array is below";
+echo "<br />";
+reverseArray($arr, 0, count($arr));
+ 
 
 ?>
+ 
